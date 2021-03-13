b97ef595438657583929c61027c84116161dded4
Brett Sullivan <brett@flaktest.com>
2021-03-13 10:25:05 +0800

    null change

--- lib/src/infrastructure/core/firestore_helpers.dart
+++ lib/src/infrastructure/core/firestore_helpers.dart
@@ -5,23 +5,27 @@ import 'package:user_repository/user_repository.dart';
 extension FirestoreX on FirebaseFirestore {
   Future<DocumentReference> userDocument() async {
     final userOption = await getIt<IAuthFacade>().getSignedInUser();
-    final user = userOption.getOrElse(() => throw NotAuthenticatedError());
+    final user =
+        userOption.getOrElse(() => throw NotAuthenticatedError());
     return FirebaseFirestore.instance
         .collection('users')
-        .doc(user.id.getOrCrash());
+        .doc(user.id!.getOrCrash());
   }
 
   CollectionReference get leagueCollection => collection('leagues');
   CollectionReference get teamCollection => collection('teams');
   CollectionReference get profileCollection => collection('users');
-  CollectionReference get teamMembersCollection => collection('teamMembers');
+  CollectionReference get teamMembersCollection =>
+      collection('teamMembers');
   CollectionReference get clubCollection => collection('clubs');
-  CollectionReference get clubMemberCollection => collection('clubMembers');
+  CollectionReference get clubMemberCollection =>
+      collection('clubMembers');
   CollectionReference get matchCollection => collection('matches');
 }
 
 extension DocumentReferenceX on DocumentReference {
   CollectionReference get userDataCollection => collection('users');
   CollectionReference get leagueCollection => collection('leagues');
-  CollectionReference get teamStatsCollection => collection('teamStats');
+  CollectionReference get teamStatsCollection =>
+      collection('teamStats');
 }
